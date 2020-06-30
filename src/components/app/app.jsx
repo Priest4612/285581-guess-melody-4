2323b323b1bc8356caccfeab39d818aeb3d3605b
priest4612 <prie4612@gmail.com>
2020-06-30 03:00:39 +0300

    Добавил тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,20 +3,22 @@ import PropTypes from 'prop-types';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
+const welcomeButtonHandler = () => {};
 
 const App = (props) => {
-  const {errorCount} = props;
+  const {errorsCount} = props;
 
   return (
     <WelcomeScreen
-      errorCount={errorCount}
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
     />
   );
 };
 
 
 App.propTypes = {
-  errorCount: PropTypes.number.isRequired,
+  errorsCount: PropTypes.number.isRequired,
 };
 
 
